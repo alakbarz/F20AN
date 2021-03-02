883456926986a8fcb92076bf181ab0a5e876ac46
alakbarz <me@alak.bar>
2021-03-02 19:17:25 +0000

    Added output to program to show src and dst IP and PORT

--- mitnick.py
+++ mitnick.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+
+# Comments in quotation marks are from the scapy documentation
+
 # ’U’: URG bit
 # ’A’: ACK bit
 # ’P’: PSH bit
@@ -14,14 +17,18 @@ x_port = 514
 srv_ip = "10.0.2.15"
 srv_port = 1023
 
-ip = IP(src=srv_ip, dst=x_ip)
-tcp = TCP(sport=srv_port, dport=x_port)
-tcp.flags = "S"
+def send_ACK(sip, dip, sport, dport):
+	ip = IP(src=sip, dst=dip)
+	tcp = TCP(sport=sport, dport=dport)
+	tcp.flags = "S"
+	packet = ip/tcp
 
-packet = ip/tcp
+	if tcp.flags == "S":
+		print("Sending SYN to {}:{} from {}:{}".format(sip, sport, dip, dport))
+		# ls(packet) # "List available layers, or infos on a given layer class or name."
+		send(packet)
+	else:
+		print("TCP flag not set to SYN in send_ACK function. Quitting...")
+		quit()
 
-if tcp.flags == "S":
-	print("Sending SYN")
-
-ls(packet)
-send(packet, verbose = 0)
+send_ACK(srv_ip, x_ip, srv_port, x_port)
